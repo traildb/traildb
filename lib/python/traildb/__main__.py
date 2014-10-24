b5dceefb3514cbca5183db06366b85b564d3965d
Jared Flatow <jflatow@gmail.com>
2014-10-23 18:25:53 -0700

    make a.tdb the default name

--- lib/python/traildb/__main__.py
+++ lib/python/traildb/__main__.py
@@ -1,5 +1,5 @@
 import sys
 import traildb
 
-for cookie, trail in traildb.TrailDB(sys.argv[1]).crumbs():
+for cookie, trail in traildb.TrailDB(*(sys.argv[1:] or ['a.tdb'])).crumbs():
     print cookie, list(trail)
