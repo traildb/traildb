c2bc5d0a6503f1890fb44f0978639288ec8a6f82
Mikko Juola <mikko.juola@adroll.com>
2015-12-15 13:22:19 -0800

    Fix warnings in one of the tests.
    random() is not as standard as I thought. Use rand() instead.

--- testing/c-tests/cookie_raw_cookie_hex_should_be_identity.c
+++ testing/c-tests/cookie_raw_cookie_hex_should_be_identity.c
@@ -17,14 +17,14 @@ int main(int argc, char** argv)
         uint8_t uuid2[16];
 
         for ( int i2 = 0; i2 < 16; ++i2 ) {
-            uuid[i2] = random();
+            uuid[i2] = rand();
         }
         tdb_cookie_hex(uuid, hex_uuid);
         tdb_cookie_raw(hex_uuid, uuid2);
         assert(!memcmp(uuid, uuid2, 16));
 
         for ( int i2 = 0; i2 < 32; ++i2 ) {
-            hex_uuid[i2] = random() % 16;
+            hex_uuid[i2] = rand() % 16;
             hex_uuid[i2] += '0';
             if ( hex_uuid[i2] > '9' ) {
                 hex_uuid[i2] -= ('9'+1);
