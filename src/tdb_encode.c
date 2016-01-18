a57a7f73cd7f5d608de5011f12088dd431902eb5
Ville Tuulos <ville.tuulos@adroll.com>
2016-01-18 14:52:58 -0800

    replace GNU mkostemp() with a more standard mkstemp() that works on OS X

--- src/tdb_encode.c
+++ src/tdb_encode.c
@@ -1,5 +1,5 @@
-#define _GNU_SOURCE /* mkostemp() */
 
+#define _DEFAULT_SOURCE /* mkstemp */
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -422,7 +422,7 @@ tdb_error tdb_encode(tdb_cons *cons, tdb_item *items)
     TDB_TIMER_START
 
     TDB_PATH(grouped_path, "%s/tmp.grouped.XXXXXX", root);
-    if ((fd = mkostemp(grouped_path, 0)) == -1){
+    if ((fd = mkstemp(grouped_path)) == -1){
         ret = TDB_ERR_IO_OPEN;
         goto done;
     }
