traildb/traildb

commit a1bf6ee2eb2bb57e0f2beb159a7d0d3e8a0715a7

add needed macros to compile

tdb/op_make.c
@@ -6,6 +6,12 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#undef JUDYERROR
+#define JUDYERROR(CallerFile, CallerLine, JudyFunc, JudyErrno, JudyErrID) \
+{                                                                         \
+   if ((JudyErrno) != JU_ERRNO_NOMEM)                                     \
+       goto out_of_memory;                                                \
+}
 #include <Judy.h>
 #include "jsmn/jsmn.h"
 
@@ -54,6 +60,9 @@ static void populate_fields(const char *fieldstr,
         if (++opt->num_fields == TDB_MAX_NUM_FIELDS)
             DIE("Too many fields");
     }
+
+out_of_memory:
+    DIE("Out of memory.");
 }
 
 static void init_fields_from_header(FILE *input, struct tdbcli_options *opt)
@@ -218,6 +227,9 @@ static void parse_csv(tdb_cons *cons,
         return;
     else
         DIE("Premature end of input or out of memory.\n");
+
+out_of_memory:
+    DIE("Out of memory.");
 }
 
 static Pvoid_t init_json_fields(const struct tdbcli_options *opt)
@@ -239,6 +251,9 @@ static Pvoid_t init_json_fields(const struct tdbcli_options *opt)
         *ptr = i + 2;
     }
     return json_fields;
+
+out_of_memory:
+    DIE("Out of memory.");
 }
 
 static void parse_json(tdb_cons *cons,
@@ -348,6 +363,9 @@ static void parse_json(tdb_cons *cons,
         return;
     else
         DIE("Premature end of input or out of memory.");
+
+out_of_memory:
+    DIE("Out of memory.");
 }
 
 int op_make(struct tdbcli_options *opt)

commit 97a142afc2bff61117c6de693e0844bb3bf5da61

fix tdb-cli regression after commit a1bf6ee

tdb/op_make.c
@@ -60,6 +60,7 @@ static void populate_fields(const char *fieldstr,
         if (++opt->num_fields == TDB_MAX_NUM_FIELDS)
             DIE("Too many fields");
     }
+    return;
 
 out_of_memory:
     DIE("Out of memory.");
@@ -227,6 +228,8 @@ static void parse_csv(tdb_cons *cons,
         return;
     else
         DIE("Premature end of input or out of memory.\n");
+
+    return;
 
 out_of_memory:
     DIE("Out of memory.");