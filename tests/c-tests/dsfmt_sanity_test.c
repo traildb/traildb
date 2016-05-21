b7fd1491a25c12b229eaea5cf76f6af5736b8a7d
Ville Tuulos <ville.tuulos@adroll.com>
2016-05-20 20:05:17 -0700

    fix a warning in dsfmt_sanity_test

--- tests/c-tests/dsfmt_sanity_test.c
+++ tests/c-tests/dsfmt_sanity_test.c
@@ -14,13 +14,9 @@ int main(int argc, char **argv)
 {
     // we are initializing an array of dsfmt_t 's
     // with a few close values and then generating values
-    int size = 5;
     uint32_t seed = 4321;
     dsfmt_t dsfmt;
-
     dsfmt_init_gen_rand(&dsfmt, seed);
-
     dsfmt_genrand_close_open(&dsfmt);
-    
     return 0;
 }
